a0de650bc170b6a98439049d7c36fc6067a4fd4a
eduardoRduraes <eduardoduras.bsi@gmail.com>
2020-11-11 01:14:43 -0300

    commit com correção do desafio 05

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -6,6 +6,12 @@ interface Balance {
   total: number;
 }
 
+interface CreateTransactionDTO {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
+
 class TransactionsRepository {
   private transactions: Transaction[];
 
@@ -14,15 +20,40 @@ class TransactionsRepository {
   }
 
   public all(): Transaction[] {
-    // TODO
+    return this.transactions;
   }
 
   public getBalance(): Balance {
-    // TODO
+    const income = this.transactions.reduce((prev, curr) => {
+      if (curr.type === 'income') return prev + curr.value;
+      return prev;
+    }, 0);
+
+    const outcome = this.transactions.reduce((prev, curr) => {
+      if (curr.type === 'outcome') return prev + curr.value;
+      return prev;
+    }, 0);
+
+    const total = income - outcome;
+
+    return { income, outcome, total };
   }
 
-  public create(): Transaction {
-    // TODO
+  public create({ title, value, type }: CreateTransactionDTO): Transaction {
+    const { total } = this.getBalance();
+
+    if (total < value && type === 'outcome')
+      throw new Error('insufficient funds');
+
+    const transaction = new Transaction({
+      title,
+      value,
+      type,
+    });
+
+    this.transactions.push(transaction);
+
+    return transaction;
   }
 }
 
